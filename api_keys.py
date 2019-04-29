e7408d3678e69952bb085575797c52bf9458bc98
Maira Gajda <gajda@umich.edu>
2019-04-29 17:43:14 -0400

    updated to current
    •params to search through newsapi.org
    •function using requests to get the json dats

--- api_keys.py
+++ api_keys.py
@@ -1,2 +1,3 @@
 news_api_key =
 NYT_api_key =
+newsAPIKey = "53e7d35bbc77439d87c20bd7b542cbb7" 
--- main.py
+++ main.py
@@ -1,2 +1,27 @@
 import plotly
-import sqlite
+import sqlite3
+import json 
+import requests
+import api_keys
+
+topHeadlinesEndpoints = "/v2/top-headlines?"
+EverythingEndpoints = "/v2/everything?"
+baseURL = "https://newsapi.org"
+newyorktimesID = "the-new-york-times" 
+
+
+def EverythingParams(query, website):
+    parameter= {
+    "apiKey": "53e7d35bbc77439d87c20bd7b542cbb7",
+    "q": query,
+    "sources":  website, #make sure the input is in id format for the newspai key 
+    "sortBy": "publishedAt",
+    "pageSize": 100
+    }
+    return parameter
+
+def getEverything(dictofparams):
+responses = requests.get(baseURL+EverythingEndpoints, params = dictofparams)
+everything = json.loads(responses.text)
+
+return everything
